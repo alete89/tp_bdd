46e02964311483350f0e4093d0e72ce3141d4765
alete89 <maildealete@gmail.com>
2018-12-06 12:17:44 -0300

    se calcula la prima y la fecha fin

--- src/PolizaAuto.py
+++ src/PolizaAuto.py
@@ -1,14 +1,28 @@
-class PolizaAuto:
-    id: int
-    estado : int
-    productorLegajo : int
-    clienteDni : int
-    prima : float
-    fechaInicio : str
-    fechaFin : str
-    porcentajeProductor : float
+from PyQt5.QtCore import QDate
 
-    autoId : int
-    grupoRiesgoId : int
-    franquicia : float
-    
+
+class PolizaAuto:
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    id: int
+    estado: int
+    productorLegajo: int
+    clienteDni: int
+    prima: float
+    fechaInicio: str
+    fechaFin: str
+    porcentajeProductor: float
+
+    autoId: int
+    grupoRiesgoId: int
+    franquicia: float
+
+    def calcularPrima(self):
+        self.prima = self.franquicia / 12
+
+    def calcularFechaFin(self):
+        fechaFin = QDate.fromString(self.fechaInicio, "yyyy-MM-dd")
+        fechaFin = fechaFin.addYears(1)
+        self.fechaFin = fechaFin.toString("yyyy-MM-dd")
